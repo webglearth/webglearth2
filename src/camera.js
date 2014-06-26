8df04c4072b1340dba2d6fb0c5fcce593ee1df0a
Petr Sloup <slouppetr@gmail.com>
2014-06-26 19:35:59 +0200

    Bugfix for not being able to set position of the camera at certain conditions

--- src/camera.js
+++ src/camera.js
@@ -48,8 +48,8 @@ weapi.Camera.prototype.setPos = function(latitude, longitude, altitude) {
       !goog.isDef(longitude) ||
       !goog.isDef(altitude)) {
     var oldPos = this.getPos();
-    latitude = !goog.isDefAndNotNull(latitude) ? latitude : oldPos[0];
-    longitude = !goog.isDefAndNotNull(longitude) ? longitude : oldPos[1];
+    latitude = goog.isDefAndNotNull(latitude) ? latitude : oldPos[0];
+    longitude = goog.isDefAndNotNull(longitude) ? longitude : oldPos[1];
     altitude = altitude > 0 ? altitude : oldPos[2];
   }
   var carto = new Cesium.Cartographic(longitude, latitude, altitude);
